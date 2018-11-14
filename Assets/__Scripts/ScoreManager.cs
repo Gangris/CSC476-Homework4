aa37a8540d8360b4daee83ce4ce8cf3203e18ec0
Adarian Eastman <adarianeastman@gmail.com>
2018-11-14 04:27:32 -0500

    Prospector solitare now on source control

---
+++ Assets/__Scripts/ScoreManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eScoreEvent
+{
+    draw,
+    mine,
+    mineGold,
+    gameWin,
+    gameLoss
+}
+
+public class ScoreManager : MonoBehaviour
+{
+    private static ScoreManager S;
+    public static int SCORE_FROM_PREVIOUS_ROUND = 0;
+    public static int HIGH_SCORE = 0;
+
+    [Header("Set Dynamically")]
+    public int chain = 0;
+    public int scoreRun = 0;
+    public int score = 0;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("ERROR: ScoreManager.Awake(): S is already set!");
+        }
+
+        if (PlayerPrefs.HasKey("ProspectorHighScore"))
+        {
+            HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
+        }
+
+        score += SCORE_FROM_PREVIOUS_ROUND;
+        SCORE_FROM_PREVIOUS_ROUND = 0;
+    }
+
+    public static void EVENT(eScoreEvent evt)
+    {
+        try
+        {
+            S.Event(evt);
+        }
+        catch (System.NullReferenceException nre)
+        {
+            Debug.LogError("ScoreManager:EVENT() called while S = null.\n" + nre);
+        }
+    }
+
+    void Event(eScoreEvent evt)
+    {
+        switch (evt)
+        {
+            case eScoreEvent.draw:
+            case eScoreEvent.gameWin:
+            case eScoreEvent.gameLoss:
+                chain = 0;
+                score += scoreRun;
+                scoreRun = 0;
+                break;
+
+            case eScoreEvent.mine:
+                chain++;
+                scoreRun += chain;
+                break;
+
+            case eScoreEvent.mineGold:
+                chain = chain * 2;
+                scoreRun += chain;
+                break;
+        }
+
+        switch (evt)
+        {
+            case eScoreEvent.gameWin:
+                SCORE_FROM_PREVIOUS_ROUND = score;
+                print("You won this round! Round score: " + score);
+                break;
+
+            case eScoreEvent.gameLoss:
+                if (HIGH_SCORE <= score)
+                {
+                    print("You got the high score! High Score: " + score);
+                    HIGH_SCORE = score;
+                    PlayerPrefs.SetInt("ProspectorHighScore", score);
+                }
+                else
+                {
+                    print("Your final score for the game was: " + score);
+                }
+                break;
+
+            default:
+                print("score: " + score + "   scoreRun: " + scoreRun + "   chain: " + chain);
+                break;
+        }
+    }
+
+    public static int CHAIN
+    {
+        get { return S.chain; }
+    }
+
+    public static int SCORE
+    {
+        get { return S.score; }
+    }
+
+    public static int SCORE_RUN
+    {
+        get { return S.scoreRun; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+Prospector Solitaire
+Adarian Eastman
+Homework 4
+
+Other than a couple of i's l's and 1's being typo'd and causing hours of headache, the game went smoothly.
+
+Also, Microsoft's Math.Random() does not work in Unity, as Unity has an implementation. A note I keep forgetting.
+
+What I implemented was the Gold card. As the way the book suggests to implement it is vague in order of scoring, I decided that I wanted it to be a 'push your luck' aspect to the game. It rewards adding it to a longer run as it doubles your current streak instead of adding one.
+
+10% seems to be a good number for the amount of gold cards, however, I've had two games where 0 cards were gold. An improvement may be to have a minimum amount.
+
+Lastly, I still can't get WebGL builds to work on TAZ, so it is zipped in D2L.
+Crunched this out in like one sitting, so no rich git commit history.
